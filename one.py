20869d308648702bca6d1bfef5b42cba091f142d
mcabrera101 <Miguel.cabrera101@gmail.com>
2022-02-27 13:41:42 -0800

    update

--- one.py
+++ one.py
@@ -4,10 +4,10 @@ import pandas as pd
 df = pd.read_csv('HappinessData-1.csv')
 print(df)
 df_reorder = df[['City Services Availability', 'Housing Cost', 'Quality of schools', 'Community trust in local police', 'Community Maintenance', 'Availability of Community Room ', 'Unhappy/Happy']] # rearrange column here
-
+df_reorder.to_csv('HappinessData-1.csv', index=False)
 
 #Task3
 for i in df_reorder:
-    df_reorder[i] = df_reorder[i].fillna(df_reorder[i].mean())
+    df_reorder[i] = df_reorder[i].fillna(float("{:,.2f}".format(df_reorder[i].mean())))
 
 df_reorder.to_csv('HappinessData-1.csv', index=False)
