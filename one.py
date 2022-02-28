d41b03331e414c9bd32aaa71bc210d820263dab2
mcabrera101 <Miguel.cabrera101@gmail.com>
2022-02-28 11:03:31 -0800

    updated task 3 to put integers into the csv file instead of the float

--- one.py
+++ one.py
@@ -1,20 +1,26 @@
 import pandas as pd
 from scipy.stats import pearsonr
+import numpy as np
+from itertools import cycle
+
+correlation_matrix = np.empty([7, 7])
 
 #Task2
-df = pd.read_csv('HappinessData-1.csv')
-print(df)
-df_reorder = df[['City Services Availability', 'Housing Cost', 'Quality of schools', 'Community trust in local police', 'Community Maintenance', 'Availability of Community Room ', 'Unhappy/Happy']] # rearrange column here
-df_reorder.to_csv('HappinessData-1.csv', index=False)
+raw_features = pd.read_csv('HappinessData-1.csv')
+print(raw_features)
+features = raw_features[['City Services Availability', 'Housing Cost', 'Quality of schools', 'Community trust in local police', 'Community Maintenance', 'Availability of Community Room ', 'Unhappy/Happy']] # rearrange column here
+features.to_csv('HappinessData-1.csv', index=False)
 
 #Task3
-for i in df_reorder:
-    df_reorder[i] = df_reorder[i].fillna(int(df_reorder[i].mean()))
+for feature in features:
+    features[feature] = features[feature].fillna(int(features[feature].mean()))
+    #if feature+1 != features[-1]:
+        #corr, _ = pearsonr(features[feature], features[feature+1])
 
-df_reorder.to_csv('HappinessData-1.csv', index=False)
+
+
+features.to_csv('HappinessData-1.csv', index=False)
 
 #task4
-corr, _ = pearsonr(df_reorder['Housing Cost'], df_reorder['Unhappy/Happy'])
-#for i
-#corr[i] = pearsonr(df_reorder["City Services Availability"],df_reorder["Housing Cost"])
-print(corr)
+
+#print(corr)
