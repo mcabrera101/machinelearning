c6cb8e5b3c72af3f55d4bfbc087e16350d430876
mcabrera101 <Miguel.cabrera101@gmail.com>
2022-02-28 10:19:24 -0800

    updated task 3 to put integers into the csv file instead of the float

--- one.py
+++ one.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from scipy.stats import pearsonr
 
 #Task2
 df = pd.read_csv('HappinessData-1.csv')
@@ -11,3 +12,9 @@ for i in df_reorder:
     df_reorder[i] = df_reorder[i].fillna(int(df_reorder[i].mean()))
 
 df_reorder.to_csv('HappinessData-1.csv', index=False)
+
+#task4
+corr, _ = pearsonr(df_reorder['Housing Cost'], df_reorder['Unhappy/Happy'])
+#for i
+#corr[i] = pearsonr(df_reorder["City Services Availability"],df_reorder["Housing Cost"])
+print(corr)
