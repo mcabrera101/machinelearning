32c61f0893ab6942c5e470b8861b40583a088412
mcabrera101 <Miguel.cabrera101@gmail.com>
2022-03-01 22:17:25 -0800

    made a file for the knn model

---
+++ knn.py
@@ -0,0 +1,10 @@
+
+def distance
+
+class knn:
+    def __init__(self, k = 5):
+        self.k = k
+
+    def fit(self):
+        pass
+    def predict(self, ):
--- one.py
+++ one.py
@@ -1,40 +1,36 @@
 import pandas as pd
 from scipy.stats import pearsonr
 import numpy as np
-from itertools import cycle
 
-correlation_matrix = np.empty([7, 7])
+
+correlation_matrix = np.empty([7, 7], dtype = object)
 
 #Task2
 raw_features = pd.read_csv('HappinessData-1.csv')
 print(raw_features)
-features = raw_features[['City Services Availability', 'Housing Cost', 'Quality of schools', 'Community trust in local police', 'Community Maintenance', 'Availability of Community Room ', 'Unhappy/Happy']] # rearrange column here
+features = raw_features[['City Services Availability', 'Housing Cost', 'Quality of schools', 'Community trust in local police',
+                         'Community Maintenance', 'Availability of Community Room ', 'Unhappy/Happy']] # rearrange column here
 features.to_csv('HappinessData-1.csv', index=False)
 
-#Task3
-last_column = features.iloc[-1, :]
-#print(last_column)
+#Task 3 and 4
+
+index = 0
 for feature in features:
     it = iter(features)
     #print(it)
     features[feature] = features[feature].fillna(int(features[feature].mean()))
-    try:
-        while (True):
-
-            correlation_matrix[feature,], _ = pearsonr(features[feature], features[next(it)])
-    except StopIteration:
-        print(corr)
-    """""
-    if feature != 'Unhappy/Happy':
-       corr, _ = pearsonr(features[feature], features[next(it)])
-    if feature != "Housing Cost":
-        corr, _ = pearsonr(features["Housing Cost"], features[next(it)])
-        print(corr)
-"""
-
-
+    correlation_matrix[0, index], _ = pearsonr(features["City Services Availability"], features[feature])
+    correlation_matrix[1, index], _ = pearsonr(features["Housing Cost"], features[feature])
+    correlation_matrix[2, index], _ = pearsonr(features['Quality of schools'], features[feature])
+    correlation_matrix[3, index], _ = pearsonr(features['Community trust in local police'], features[feature])
+    correlation_matrix[4, index], _ = pearsonr(features['Community Maintenance'], features[feature])
+    correlation_matrix[5, index], _ = pearsonr(features['Availability of Community Room '], features[feature])
+    correlation_matrix[6, index], _ = pearsonr(features['Unhappy/Happy'], features[feature])
+    index = index + 1
 
 features.to_csv('HappinessData-1.csv', index=False)
+
+
 
 #task4
 
