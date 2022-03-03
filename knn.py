df35b5de37f1883cf79a30b2febb29407743e52c
mcabrera101 <Miguel.cabrera101@gmail.com>
2022-03-02 23:50:19 -0800

    filled some of the model in

--- knn.py
+++ knn.py
@@ -7,14 +7,62 @@ class KNN:
     """Machinge learning algorything using the nearest neighbor technique"""
     def __init__(self, k=3):
         self.k = k
+        self.independents = 0
+        self.dependent = 0
 
-    def euclidean_distance(x1, x2):
-        return np.sqrt(np.sum((x1 - x2) ** 2))
-    def store(self,trainingSet):
-        self.independent = trainingSet['City Services Availability', 'Housing Cost', 'Quality of schools', 'Community trust in local police',
-                         'Community Maintenance', 'Availability of Community Room ']
-        self.dependent = trainingSet['Unhappy/Happy']
-    def predict(self,test):
-        pass
-    def asess(self,prediction,test):
+    def euclidean_distance(self, test, train):
+        distance = 0
+        index = 0
+
+        for i in range(len(test)):
+            index = train[0]
+            if i != 0:
+                a = test[i]
+                b = train[i]
+                distance = distance + np.square(a - b)
+
+        return np.sqrt(distance), index
+
+    def store(self, trainx, trainy):
+        self.independents = trainx
+        self.dependent = trainy
+
+    def predict(self, test):
+        # Predict the class label based on the test data 
+        #prediction = [self._predict(data) for name, data in test.items()]
+        # find the distance between the points
+        distances = np.empty([28, 112], dtype = object)
+        col = 0
+        row = 0
+        for i in test.itertuples():
+            col = 0
+            for j in self.independents.itertuples():
+                distances[row][col] = self.euclidean_distance(i, j)
+                col = col + 1
+            row = row + 1
+        # sort distances and return the index of the closest k neighbors
+        distances_sorted = np.sort(distances)
+
+
+        d = np.delete(distances_sorted, np.s_[self.k:111], 1)
+        k_index = []
+        for i in d:
+            for s in i:
+                k_index.append(s[1])
+
+        '''
+        index = 0
+        temp = 1000
+        indecies = []
+        for i in range(row):
+            for j in range(col):
+                if temp > distances[i][j]:
+                    temp = distances[i][j]
+                    index = j
+            indecies[i] = j
+        '''
+        return None
+        '''
+
+    def asses(self, prediction, test):
         pass
--- one.py
+++ one.py
@@ -1,3 +1,5 @@
+# Miguel Cabrera
+
 import pandas as pd
 from scipy.stats import pearsonr
 import numpy as np
@@ -35,10 +37,50 @@ for feature in features:
 features.to_csv('HappinessData-1.csv', index=False)
 print(features.shape)
 
-plt.figure()
+'''plt.figure()
 plt.scatter(features['City Services Availability'], features['Housing Cost'], c=features['Unhappy/Happy'], cmap=cmap,
             edgecolor='k', s=20)
-plt.show()
-d = eucledi
+plt.show()'''
 k = 5
+
+trainI, testI, trainD, testD = train_test_split(features.iloc[:, 0:6], features.iloc[:, 6], test_size=0.2, random_state=777)
+'''
+train = features.iloc[29:140, :]
+trainI = features.iloc[29:140, 0:5]
+trainD = features.iloc[29:140, 6]
+testI = features.iloc[1:28, 0:5]
+testD = features.iloc[1:28, 6]
+'''
 model = KNN(k)
+model.store(trainI, trainD)
+prediction = model.predict(testI)
+#d = model.euclidean_distance(features['City Services Availability'], features['Housing Cost'])
+print(prediction)
+
+#Task 6
+knn = KNeighborsClassifier(n_neighbors=5)
+knn.fit(trainI, trainD)
+knn.predict(testI)
+
+#Testing knn with different values of k
+"""
+errorRate = np.empty([50], dtype = object)
+correct = 0
+for i in range(50):
+    model = KNN(i)
+    model.store(trainI, trainD)
+    prediction = model.predict(testI)
+    for i in range(len(train.index)):
+        if trainD == classified
+            correct = correct + 1
+    errorRate[i] = correct/len(train.index)
+
+x = range(50)
+y = errorRate
+plt.plot(x, y)
+plt.xlabel('K')
+plt.ylabel('Error Rate') 
+plt.title('Error Rate vs K') 
+plt.show()
+
+"""
